c6a6089df2a79f0637722b02d5ebb302ff9ae85e
philippe villette <pvillette@cegid.com>
2019-05-27 14:48:31 +0200

    modif du tableau

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from './models/Post.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  AppPosts: Post[] = [];
+
   title = 'blog';
+
+  ngOnInit() {
+    console.log("création des posts initiaux");
+    
+    var aPost = new Post('Mon premier post', 'le contenu de mon premier post');
+    aPost.loveIts = 1; // pour qu'il soit vert
+    this.AppPosts.push(aPost);
+
+    aPost = new Post('Mon deuxième post', 'le contenu de mon 2eme post')
+    aPost.loveIts = -1; // pour qu'il soit rouge
+    this.AppPosts.push(aPost);
+
+    aPost = new Post('Encore un post', 'le contenu de mon 3eme post');
+    this.AppPosts.push(aPost);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostListComponentComponent } from './post-list-component/post-list-component.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'post', component: PostListComponentComponent },
+  { path: '', redirectTo: 'post', pathMatch: 'full' },
+  { path: '**', redirectTo: 'post' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
